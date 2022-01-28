082a1e92b70728f5ccd741a688b2f1260b70b8fe
andrey-omsk-63 <andrey_omsk@mail.ru>
2022-01-28 06:34:37 +0600

    '2801mor'

--- src/components/Management/grid/ManagLeftGrid.tsx
+++ src/components/Management/grid/ManagLeftGrid.tsx
@@ -4,57 +4,8 @@ import Box from '@mui/material/Box';
 
 import { Tflight } from '../../../interfaceMNG.d';
 
-export interface DataMas {
-  masss: ArrMass[];
-}
-
-export interface ArrMass {
-  area: AreaM;
-  subarea: number;
-}
-
-export interface AreaM {
-  num: string;
-  nameArea: string;
-}
-
 const ManagementLeftGrid = (props: { open: boolean; tflightt: Tflight[] }) => {
   const points = props.tflightt;
-
-  //if (props.open) console.log(points.find({subarea: "1"}));
-  if (props.open) {
-    //console.log(points[0].ID);
-    // let mass: Array<Tflight> = points.filter(tflight => tflight.ID == 8);
-    // let mass: Array<Tflight> = [];
-    // mass = points.filter(tflight => tflight.area.num === '2');
-    // console.log('mass1:', mass);
-    // mass = points.filter(tflight => tflight.area.num === '2' && tflight.subarea === 2);
-    // console.log('mass2:', mass);
-    // console.log('mass0:', points);
-    console.log('points:', points);
-
-    let mass: Array<ArrMass> = [];
-    mass[0] = {
-      area: points[0].area,
-      subarea: points[0].subarea,
-      
-    };
-    let j = 0
-    for (let i = 1; i < points.length; i++) {
-      console.log('j:', j, 'i:', i, mass[j].area, points[i].area)
-      if (mass[j].area !== points[i].area || mass[j].subarea !== points[i].subarea) {
-        j++
-        mass[j] = {
-          area: points[i].area,
-          subarea: points[i].subarea,
-          
-        };
-        
-      }
-      
-    }
-    console.log('mass:', mass);
-  }
 
   const styleMG01 = {
     border: 1,
@@ -68,27 +19,81 @@ const ManagementLeftGrid = (props: { open: boolean; tflightt: Tflight[] }) => {
     padding: 0.3,
   };
 
-  const SpisAreaMLG = (props: { nom: number; name: string }) => {
+  let mass: any = [];
+  let masRab: any = [];
+  let masAreaNum: any = [];
+
+  if (props.open) {
+    masRab[0] = points[0].area.num;
+    mass[0] = {
+      areaNum: points[0].area.num,
+      areaName: points[0].area.nameArea,
+      subarea: points[0].subarea,
+    };
+    let j = 0;
+    for (let i = 1; i < points.length; i++) {
+      if (mass[j].area !== points[i].area && mass[j].subarea !== points[i].subarea) {
+        j++;
+        masRab[j] = points[i].area.num;
+        mass[j] = {
+          areaNum: points[i].area.num,
+          areaName: points[i].area.nameArea,
+          subarea: points[i].subarea,
+        };
+      }
+    }
+    //убираем дубликаты
+    masAreaNum = masRab.filter((element: any, index: any) => {
+      return masRab.indexOf(element) === index;
+    });
+
+    console.log('mass:', mass);
+    // console.log('masRab:', masRab);
+    // console.log('masAreaNum:', masAreaNum);
+  }
+
+  const SpisAreaMLG = (props: { nom: string }) => {
+    masRab = [];
+    masRab = mass.filter((mass: { areaNum: string }) => mass.areaNum === props.nom);
+    console.log('masRab1:', masRab);
+
+    const SpisSubAreaMLG = () => {
+      let resStr = [];
+      for (let i = 0; i < masRab.length; i++) {
+        resStr.push(
+          <Grid container key={Math.random()}>
+            <Grid key={Math.random()} item xs={2} sx={styleMG03}></Grid>
+            <Grid key={Math.random()} item xs={10} sx={styleMG03}>
+              Подрайон:{masRab[i].areaNum}:{masRab[i].subarea}
+            </Grid>
+          </Grid>,
+        );
+      }
+      return resStr;
+    };
+
     return (
       <>
         <Grid container>
           <Grid item xs={1} sx={styleMG03}></Grid>
           <Grid item xs={11} sx={styleMG03}>
-            Район:{props.nom}&nbsp;&nbsp;&nbsp;&nbsp;{props.name}
+            Район:{masRab[0].areaNum}&nbsp;&nbsp;&nbsp;&nbsp;{masRab[0].areaName}
           </Grid>
+          {SpisSubAreaMLG()}
         </Grid>
-        <Grid container>
-          <Grid item xs={2} sx={styleMG03}></Grid>
-          <Grid item xs={10} sx={styleMG03}>
-            Подрайон:{props.nom}:1
-          </Grid>
-        </Grid>
-
       </>
     );
   };
 
   const SpisMLG = () => {
+    const SpisAreaGlob = () => {
+      let resStr = [];
+      for (let i = 0; i < masAreaNum.length; i++) {
+        resStr.push(<SpisAreaMLG nom={masAreaNum[i]} key={Math.random()} />);
+      }
+      return resStr;
+    };
+
     return (
       <>
         <Grid container>
@@ -96,12 +101,7 @@ const ManagementLeftGrid = (props: { open: boolean; tflightt: Tflight[] }) => {
             <b>Регион</b>&nbsp;&nbsp;&nbsp;&nbsp;<b>{points[0].region.nameRegion}</b>
           </Grid>
         </Grid>
-        <SpisAreaMLG nom={Number(points[0].area.num)} name={points[0].area.nameArea} />
-        {/* <SpisAreaMLG nom={2} name={'Вторая половина'} />
-        <SpisAreaMLG nom={3} name={'Третий кусок'} />
-        <SpisAreaMLG nom={4} name={'Четвёртый кусок'} />
-        <SpisAreaMLG nom={5} name={'Жопка'} /> */}
-
+        {SpisAreaGlob()}
       </>
     );
   };
@@ -110,11 +110,7 @@ const ManagementLeftGrid = (props: { open: boolean; tflightt: Tflight[] }) => {
     <>
       <Grid item xs={2.5} sx={styleMG01}>
         <Box sx={{ overflowX: 'auto', height: '94.3vh', border: 0 }}>
-          {props.open && (
-            <div>
-              <SpisMLG />
-            </div>
-          )}
+          {props.open && <SpisMLG />}
         </Box>
       </Grid>
     </>
@@ -122,3 +118,10 @@ const ManagementLeftGrid = (props: { open: boolean; tflightt: Tflight[] }) => {
 };
 
 export default ManagementLeftGrid;
+
+//console.log(points[0].ID);
+// let mass: Array<Tflight> = points.filter(tflight => tflight.ID == 8);
+// let mass: Array<Tflight> = [];
+// mass = points.filter(tflight => tflight.area.num === '2');
+// console.log('mass1:', mass);
+// mass = points.filter(tflight => tflight.area.num === '2' && tflight.subarea === 2);
