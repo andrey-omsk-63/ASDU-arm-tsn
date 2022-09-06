ed81d89cff4ec16d8a80534f85c28191c9728ca1
andrey-omsk-63 <andrey_omsk@mail.ru>
2022-09-06 19:51:47 +0600

    '0609evn'

--- src/AppServiceFunctions.tsx
+++ src/AppServiceFunctions.tsx
@@ -2,23 +2,23 @@ export const MakeInterval = (mode: number) => {
   let dat: any = [];
   switch (mode) {
     case 1:
-      dat = ["1", "5", "10", "15", "30", "60"];
+      dat = ['1', '5', '10', '15', '30', '60'];
       break;
     case 5:
-      dat = ["5", "10", "15", "30", "60"];
+      dat = ['5', '10', '15', '30', '60'];
       break;
     case 10:
-      dat = ["10", "20", "30", "60"];
+      dat = ['10', '20', '30', '60'];
       break;
     case 15:
-      dat = ["15", "30", "60"];
+      dat = ['15', '30', '60'];
       break;
     case 20:
-      dat = ["20", "60"];
+      dat = ['20', '60'];
       break;
     default:
-      console.log("В БД некорректный интервал");
+      console.log('В БД некорректный интервал');
+      dat = ['1', '5', '10', '15', '30', '60'];
   }
   return dat;
-}
-
+};
