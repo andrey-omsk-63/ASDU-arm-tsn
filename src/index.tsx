16a58d1eb13af0d2aced818127f7ab3a8c278eae
andrey-omsk-63 <andrey_omsk@mail.ru>
2026-07-02 09:16:42 +0600

    0207/0916

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom";
 //import { createStore } from 'redux';
 
 import { legacy_createStore as createStore } from "redux";
+//import reportWebVitals from "./reportWebVitals";
+
 import { rootReducer } from "./redux/rootReducer";
 import { Provider } from "react-redux";
 
@@ -16,5 +18,10 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
